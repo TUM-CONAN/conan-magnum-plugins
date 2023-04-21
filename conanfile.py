430491f19424beee23876a3ee7101d5508effcdd
Ulrich Eck <ulrich.eck@tum.de>
2023-04-21 09:55:47 +0200

    migrated magnum-plugins

--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
+from conan.tools.scm import Git
+from conan.tools.files import load, update_conandata, copy, collect_libs, get, replace_in_file, patch
+from conan.tools.microsoft.visual import check_min_vs
+from conan.tools.system.package_manager import Apt
 import os
+
+
 
 def sort_libs(correct_order, libs, lib_suffix='', reverse_result=False):
     # Add suffix for correct string matching
@@ -23,18 +30,16 @@ def sort_libs(correct_order, libs, lib_suffix='', reverse_result=False):
 class LibnameConan(ConanFile):
     name = "magnum-plugins"
     version = "2020.06"
-    description =   "Magnum-Plugins — Lightweight and modular C++11/C++14 \
+    description = "Magnum-Plugins — Lightweight and modular C++11/C++14 \
                     graphics middleware for games and data visualization"
     # topics can get used for searches, GitHub topics, Bintray tags etc. Add here keywords about the library
     topics = ("conan", "corrade", "graphics", "rendering", "3d", "2d", "opengl")
-    url = "https://github.com/ulricheck/conan-magnum-plugins"
+    url = "https://github.com/TUM-CONAN/conan-magnum-plugins"
     homepage = "https://magnum.graphics"
     author = "ulrich eck (forked on github)"
-    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
+    license = "MIT"
     exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt", "patches/macos_jsonhpp_fix.diff"]
-    generators = "cmake"
-    short_paths = True  # Some folders go out of the 260 chars path length scope (windows)
 
     # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
@@ -93,65 +98,6 @@ class LibnameConan(ConanFile):
         "with_tinygltfimporter": False,
     }
 
-    # Custom attributes for Bincrafters recipe conventions
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
-
-    requires = (
-        "magnum/2020.06@camposs/stable"
-    )
-
-    def system_package_architecture(self):
-        if tools.os_info.with_apt:
-            if self.settings.arch == "x86":
-                return ':i386'
-            elif self.settings.arch == "x86_64":
-                return ':amd64'
-            elif self.settings.arch == "armv6" or self.settings.arch == "armv7":
-                return ':armel'
-            elif self.settings.arch == "armv7hf":
-                return ':armhf'
-            elif self.settings.arch == "armv8":
-                return ':arm64'
-
-        if tools.os_info.with_yum:
-            if self.settings.arch == "x86":
-                return '.i686'
-            elif self.settings.arch == 'x86_64':
-                return '.x86_64'
-        return ""
-
-    # def system_requirements(self):
-    #     # Install required OpenGL stuff on linux
-    #     if tools.os_info.is_linux:
-    #         if tools.os_info.with_apt:
-    #             installer = tools.SystemPackageTool()
-
-    #             packages = []
-    #             if self.options.target_gl:
-    #                 packages.append("libgl1-mesa-dev")
-    #             if self.options.target_gles:
-    #                 packages.append("libgles1-mesa-dev")
-
-    #             arch_suffix = self.system_package_architecture()
-    #             for package in packages:
-    #                 installer.install("%s%s" % (package, arch_suffix))
-
-    #         elif tools.os_info.with_yum:
-    #             installer = tools.SystemPackageTool()
-
-    #             arch_suffix = self.system_package_architecture()
-    #             packages = []
-    #             if self.options.target_gl:
-    #                 packages.append("mesa-libGL-devel")
-    #             if self.options.target_gles:
-    #                 packages.append("mesa-libGLES-devel")
-
-    #             for package in packages:
-    #                 installer.install("%s%s" % (package, arch_suffix))
-    #         else:
-    #             self.output.warn("Could not determine package manager, skipping Linux system requirements installation.")
-
     def config_options(self):
         if self.settings.os == 'Windows':
             del self.options.fPIC
@@ -162,66 +108,84 @@ class LibnameConan(ConanFile):
         # https://github.com/mosra/magnum/issues/304#issuecomment-451768389
 
         if self.options.with_assimpimporter:
-            self.options['magnum'].add_option('with_anyimageimporter', True)
+            self.options['magnum']['with_anyimageimporter'] = True
 
     def requirements(self):
+        self.requires("magnum/2020.06@camposs/stable")
         if self.options.with_assimpimporter:
-            self.requires("assimp/4.1.0@camposs/stable")
+            self.requires("assimp/5.2.2")
+
+    def export(self):
+        update_conandata(self, {"sources": {
+            "commit": "v{}".format(self.version),
+            "url": "https://github.com/mosra/magnum-plugins.git"
+            }}
+            )
 
     def source(self):
-        source_url = "https://github.com/mosra/magnum-plugins"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
-        extracted_dir = self.name + "-" + self.version
+        git = Git(self)
+        sources = self.conan_data["sources"]
+        git.clone(url=sources["url"], target=self.source_folder)
+        git.checkout(commit=sources["commit"])
+        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+            "find_package(Magnum REQUIRED)",
+            "cmake_policy(SET CMP0074 NEW)\nfind_package(Magnum REQUIRED)")
 
-        # Rename to "source_subfolder" is a convention to simplify later steps
-        os.rename(extracted_dir, self._source_subfolder)
+        # patch(self, self.source_folder, os.path.join("patches", "macos_jsonhpp_fix.diff"))
 
-        tools.replace_in_file(os.path.join(self._source_subfolder, "src", "MagnumPlugins", "AssimpImporter", "CMakeLists.txt"),
-            "target_link_libraries(AssimpImporter PUBLIC Magnum::Trade Assimp::Assimp",
-            "target_link_libraries(AssimpImporter PUBLIC Magnum::Trade CONAN_PKG::assimp")
-        tools.replace_in_file(os.path.join(self._source_subfolder, "src", "MagnumPlugins", "AssimpImporter", "CMakeLists.txt"),
-            "find_package(Assimp REQUIRED)",
-            "")
-
-    def _configure_cmake(self):
-        cmake = CMake(self)
+    def generate(self):
+        tc = CMakeToolchain(self)
 
         def add_cmake_option(option, value):
             var_name = "{}".format(option).upper()
             value_str = "{}".format(value)
-            var_value = "ON" if value_str == 'True' else "OFF" if value_str == 'False' else value_str 
-            cmake.definitions[var_name] = var_value
+            var_value = "ON" if value_str == 'True' else "OFF" if value_str == 'False' else value_str
+            tc.variables[var_name] = var_value
 
         for option, value in self.options.items():
             add_cmake_option(option, value)
 
-        # Magnum uses suffix on the resulting 'lib'-folder when running cmake.install()
-        # Set it explicitly to empty, else Magnum might set it implicitly (eg. to "64")
+        # Corrade uses suffix on the resulting 'lib'-folder when running cmake.install()
+        # Set it explicitly to empty, else Corrade might set it implicitly (eg. to "64")
         add_cmake_option("LIB_SUFFIX", "")
 
         add_cmake_option("BUILD_STATIC", not self.options.shared)
         add_cmake_option("BUILD_STATIC_PIC", not self.options.shared and self.options.get_safe("fPIC"))
+        corrade_root = self.dependencies["corrade"].package_folder
+        tc.variables["Corrade_ROOT"] = corrade_root
+        magnum_root = self.dependencies["magnum"].package_folder
+        tc.variables["Magnum_ROOT"] = magnum_root
+        assimp_root = self.dependencies["assimp"].package_folder
+        tc.variables["Assimp_ROOT"] = assimp_root
 
-        cmake.configure(build_folder=self._build_subfolder)
+        tc.generate()
 
-        return cmake
+        deps = CMakeDeps(self)
+        deps.set_property("magnum", "cmake_find_mode", "none")
+        deps.set_property("corrade", "cmake_find_mode", "none")
+        deps.set_property("assimp", "cmake_find_mode", "module")
+        deps.set_property("assimp", "cmake_file_name", "Assimp")
+        deps.set_property("assimp", "cmake_target_name", "Assimp::Assimp")
+        deps.generate()
+
+    def layout(self):
+        cmake_layout(self, src_folder="source_subfolder")
 
     def build(self):
-        tools.patch(self._source_subfolder, "patches/macos_jsonhpp_fix.diff")
-        cmake = self._configure_cmake()
-        cmake.verbose = True
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        copy(self, pattern="LICENSE", dst="licenses", src=self.source_folder)
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = collect_libs(self)
 
         if self.settings.os == "Windows":
-            if self.settings.compiler == "Visual Studio":
+            if self.settings.compiler == "msvc":
                 if not self.options.shared:
                     self.cpp_info.libs.append("OpenGL32.lib")
             else:
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -2,13 +2,42 @@
 # -*- coding: utf-8 -*-
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
-from conans import ConanFile, CMake
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
+from conan.tools.build import can_run
 import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        corrade_root = self.dependencies["corrade"].package_folder
+        tc.variables["Corrade_ROOT"] = corrade_root
+        magnum_root = self.dependencies["magnum"].package_folder
+        tc.variables["Magnum_ROOT"] = magnum_root
+        magnum_plugins_root = self.dependencies["magnum-plugins"].package_folder
+        tc.variables["MagnumPlugins_ROOT"] = magnum_plugins_root
+        assimp_root = self.dependencies["assimp"].package_folder
+        tc.variables["Assimp_ROOT"] = assimp_root
+        self.output.info("Assimp_ROOT: {}".format(assimp_root))
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.set_property("corrade", "cmake_find_mode", "none")
+        deps.set_property("magnum", "cmake_find_mode", "none")
+        deps.set_property("magnum-plugins", "cmake_find_mode", "none")
+        deps.set_property("assimp", "cmake_find_mode", "module")
+        deps.set_property("assimp", "cmake_file_name", "Assimp")
+        deps.set_property("assimp", "cmake_target_name", "Assimp::Assimp")
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
@@ -16,5 +45,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        bin_path = os.path.join("bin", "test_package")
-        self.run(bin_path, run_environment=True)
+        if can_run(self):
+            cmd = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(cmd, env="conanrun")
